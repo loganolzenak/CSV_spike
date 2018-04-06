871cb5d38c4b18701e7a07f0fbb96fe4325619f0
Logan Olzenak <logan.olzenak@gmail.com>
2018-04-06 09:23:45 -0500

    devs set up

---
+++ csv.js
@@ -0,0 +1,27 @@
+let createCSV = () =>{
+    console.log( 'in createCSV' );
+    let csvString = '';
+    csvString += document.getElementById( 'input0' ).value + ',';
+    csvString += document.getElementById( 'input1' ).value + ',';
+    csvString += document.getElementById( 'input2' ).value + ',';
+    csvString += document.getElementById( 'input3' ).value + '\n';
+    
+    csvString += document.getElementById( 'input4' ).value + ',';
+    csvString += document.getElementById( 'input5' ).value + ',';
+
+    // csvString += document.getElementById( 'input0' ).value + ',';
+    // csvString += document.getElementById( 'input1' ).value + ',';
+    // csvString += document.getElementById( 'input2' ).value + ',';
+    // csvString += document.getElementById( 'input3' ).value + '\n';
+    console.log( 'creating csv with string:', csvString );
+    downloadCsv( csvString, 'test.csv' );
+}
+
+let downloadCsv = ( string, fileName ) => {  
+    let data = encodeURI( 'data:text/csv;charset=utf-8,' + string );
+    link = document.createElement( 'a' );
+    link.setAttribute( 'href', data );
+    link.setAttribute( 'download', fileName );
+    link.click();
+}
+
