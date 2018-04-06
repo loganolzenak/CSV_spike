34167d023b132f4fe9ec7eb94df324ed867b5167
Michael Gregoire <mdgregoire@gmail.com>
2018-04-06 14:20:38 -0500

    got db info back and exported it as a csv

--- server/public/scripts/services/csv.service.js
+++ server/public/scripts/services/csv.service.js
@@ -1,27 +1,66 @@
 console.log('in service first');
 
-myApp.service('CSVService', ['$route', function($route){
+myApp.service('CSVService', ['$route', '$http', function($route, $http){
   let self = this;
+  self.export = {};
+  self.csvString = ''
 console.log('in csv service');
 
 self.createCSV = function(){
     console.log( 'in createCSV' );
     let csvString = '';
-    csvString += document.getElementById( 'input0' ).value + ',';
-    csvString += document.getElementById( 'input1' ).value + ',';
-    csvString += document.getElementById( 'input2' ).value + ',';
-    csvString += document.getElementById( 'input3' ).value + '\n';
+    self.displayDB()
+  }
 
-    csvString += document.getElementById( 'input4' ).value + ',';
-    csvString += document.getElementById( 'input5' ).value + ',';
+self.displayDB = function (){
+  console.log('in display DB');
+  $http({
+    method: 'GET',
+    url: '/csv'
+  }).then(function(response){
+    self.export.response = response.data.rows;
+    console.log(self.export);
+    self.createStringForCSV(self.export);
+  }).catch(function(error){
+    console.log('error in get DB', error);
+  })
+}
+
+self.createStringForCSV = function(taco) {
+console.log('in createStringForCSV', taco);
+let objectLength = Object.keys(taco.response[0]).length;
+console.log(objectLength, 'objectLength');
+
+self.csvString += 'transaction_id, charity_id, user_id, amount, date\n';
+
+for (i=0; i<taco.response.length; i++){
+  self.csvString += taco.response[i].id + ',';
+  self.csvString += taco.response[i].charity_id + ',';
+  self.csvString += taco.response[i].user_id + ',';
+  self.csvString += taco.response[i].amount + ',';
+  self.csvString += taco.response[i].date_posted + '\n';
+}
+console.log( 'creating csv with string:', self.csvString );
+self.downloadCsv( self.csvString, 'test.csv' );
+
+}
+//end createStringForCSV
+
 
     // csvString += document.getElementById( 'input0' ).value + ',';
     // csvString += document.getElementById( 'input1' ).value + ',';
     // csvString += document.getElementById( 'input2' ).value + ',';
     // csvString += document.getElementById( 'input3' ).value + '\n';
-    console.log( 'creating csv with string:', csvString );
-    self.downloadCsv( csvString, 'test.csv' );
-}
+    //
+    // csvString += document.getElementById( 'input4' ).value + ',';
+    // csvString += document.getElementById( 'input5' ).value + ',';
+
+    // csvString += document.getElementById( 'input0' ).value + ',';
+    // csvString += document.getElementById( 'input1' ).value + ',';
+    // csvString += document.getElementById( 'input2' ).value + ',';
+    // csvString += document.getElementById( 'input3' ).value + '\n';
+
+
 
 self.downloadCsv = function( string, fileName ){
     let data = encodeURI( 'data:text/csv;charset=utf-8,' + string );
--- server/routes/csv.router.js
+++ server/routes/csv.router.js
@@ -3,4 +3,17 @@ const pool = require('../modules/pool.js');
 const router = express.Router();
 console.log('in router csv');
 
+router.get('/', (request, response) => {
+  console.log('in get all');
+  pool.query('SELECT * FROM transaction_posted ORDER BY id;')
+  .then((result) => {
+    console.log('success in get', result);
+    response.send(result);
+  })
+  .catch((err) => {
+    console.log('error in get', err);
+    response.sendStatus(500);
+  })
+})
+
 module.exports = router;
